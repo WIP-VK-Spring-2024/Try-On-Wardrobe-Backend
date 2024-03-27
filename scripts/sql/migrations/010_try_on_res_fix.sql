a723d44439072f821f2f20c20961d7ed68651300
Leonid Fiksler <reddiridabl666@gmail.com>
2024-03-27 12:04:24 +0300

    Fixed on delete constraint

---
+++ scripts/sql/migrations/010_try_on_res_fix.sql
@@ -0,0 +1,14 @@
+-- +migrate Up
+alter table try_on_result
+drop constraint try_on_results_clothes_id_fkey,
+add constraint try_on_results_clothes_id_fkey
+   foreign key (clothes_id)
+   references clothes(id)
+   on delete cascade;
+
+-- +migrate Down
+alter table try_on_result
+drop constraint try_on_results_clothes_id_fkey,
+add constraint try_on_results_clothes_id_fkey
+   foreign key (clothes_id)
+   references clothes(id);
