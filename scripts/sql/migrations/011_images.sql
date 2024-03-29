918a9454bf47e01cb05cd51c2d918319504c0ec5
Leonid Fiksler <reddiridabl666@gmail.com>
2024-03-29 18:12:11 +0300

    Fixed migration

--- scripts/sql/migrations/011_images.sql
+++ scripts/sql/migrations/011_images.sql
@@ -2,7 +2,7 @@
 alter table clothes add column image varchar;
 
 update clothes
-set image = "cut/" + id::text
+set image = 'cut/' + id::text
 where image is null;
 
 alter table clothes alter column image set not null;
@@ -10,7 +10,7 @@ alter table clothes alter column image set not null;
 alter table user_images add column image varchar;
 
 update user_images
-set image = "people/" + id::text
+set image = 'people/' + id::text
 where image is null;
 
 alter table user_images alter column image set not null;
