e7740435f4df17ec240f548bbd2d57057d866743
Leonid Fiksler <reddiridabl666@gmail.com>
2024-04-13 16:54:40 +0300

    Fix

--- internal/pkg/delivery/outfits/outfit_handler.go
+++ internal/pkg/delivery/outfits/outfit_handler.go
@@ -244,15 +244,15 @@ func (h *OutfitHandler) Generate(ctx *fiber.Ctx) error {
 
 	var req domain.OutfitGenerationRequest
 	req = domain.OutfitGenerationRequest{
-		Amount:   4,
+		Amount:   3,
 		Prompt:   "something something",
 		Purposes: []string{"clothes for outdoor"},
 	}
 
-	if err := easyjson.Unmarshal(ctx.Body(), &req); err != nil {
-		middleware.LogWarning(ctx, err)
-		return app_errors.ErrBadRequest
-	}
+	// if err := easyjson.Unmarshal(ctx.Body(), &req); err != nil {
+	// 	middleware.LogWarning(ctx, err)
+	// 	return app_errors.ErrBadRequest
+	// }
 
 	req.UserID = session.UserID
 	req.Pos.IP = ctx.IP()
