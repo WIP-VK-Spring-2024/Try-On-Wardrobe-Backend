d23febdda499421cdd5b9af7ccc44141c8f7a105
Leonid Fiksler <reddiridabl666@gmail.com>
2024-04-16 23:54:59 +0300

    Switched to ux user

--- internal/middleware/session.go
+++ internal/middleware/session.go
@@ -2,12 +2,12 @@ package middleware
 
 import (
 	"context"
-	"fmt"
 	"slices"
 	"strings"
 
 	"try-on/internal/pkg/app_errors"
 	"try-on/internal/pkg/domain"
+	"try-on/internal/pkg/utils"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -38,14 +38,14 @@ func CheckSession(cfg SessionConfig) fiber.Handler {
 			return err
 		}
 
-		if err != nil {
-			LogWarning(ctx, err)
-		}
+		// if err != nil {
+		// 	LogWarning(ctx, err)
+		// }
 
 		if ok {
 			context := context.WithValue(ctx.UserContext(), sessionKey, &session)
 			ctx.SetUserContext(context)
-			fmt.Printf("log: setting session with id '%s'\n", session.ID)
+			// fmt.Printf("log: setting session with id '%s'\n", session.ID)
 		} else if slices.ContainsFunc(cfg.SecureRoutes, func(prefix string) bool {
 			return strings.HasPrefix(ctx.Path(), prefix)
 		}) {
@@ -57,16 +57,17 @@ func CheckSession(cfg SessionConfig) fiber.Handler {
 }
 
 func Session(ctx *fiber.Ctx) *domain.Session {
-	// userID, _ := utils.ParseUUID("2a78df8a-0277-4c72-a2d9-43fb8fef1d2c")
+	// userID, _ := utils.ParseUUID("2a78df8a-0277-4c72-a2d9-43fb8fef1d2c") // first account
+	userID, _ := utils.ParseUUID("7eef49f3-9b52-4dc8-bcd6-c40c6eb966bc") // ux account
 
-	// return &domain.Session{
-	// 	UserID: userID,
-	// }
-
-	value := ctx.UserContext().Value(sessionKey)
-	session, ok := value.(*domain.Session)
-	if !ok {
-		return nil
+	return &domain.Session{
+		UserID: userID,
 	}
-	return session
+
+	// value := ctx.UserContext().Value(sessionKey)
+	// session, ok := value.(*domain.Session)
+	// if !ok {
+	// 	return nil
+	// }
+	// return session
 }
