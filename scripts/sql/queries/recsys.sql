9fc114704434e8fc9fc8521a19c8c0e8c51cfae4
Leonid Fiksler <reddiridabl666@gmail.com>
2024-05-02 08:53:22 +0300

    Fix to ratings value check

--- scripts/sql/queries/recsys.sql
+++ scripts/sql/queries/recsys.sql
@@ -1,8 +1,9 @@
 -- name: GetClothesTensors :many
 select
-    outfits.id,
+    outfits.id as outfit_id,
     outfits.user_id,
-    array_agg(cv.tensor) as clothes
+    array_agg(cv.clothes_id)::uuid[] as clothes_id,
+    array_agg(cv.tensor)::bytea[] as clothes_tensor
 from outfits
 join clothes_vector cv on outfits.transforms ? cv.clothes_id::text
 where outfits.privacy = 'public'
